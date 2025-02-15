0050fafe3322dad17a10e0fad512ca8d2a0ea614
¨WendellReis¨ <wendellreismatias@gmail.com>
2025-02-15 13:57:07 -0300

    Leitura e exibicao

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
-def main():
-    print("testing...")
+import sys
+import json
+import os.path
+
+class TM:
+    def __init__(self, args):
+        with open(args) as arc:
+            data = json.load(arc)
+        self.Q = data['estados']
+        self.E = data['alfabeto']
+        self.T = data['simbolos']
+        self.TR = data['transicoes']
+        self.q0 = data['estado_inicial']
+        self.QACC = data['estados_aceitacao']
+        self.QREJ = data['estados_rejeicao']
+        self.words = data['palavras']
+        self.inicialize()
+
+    def inicialize(self):
+        self.tape = ['$']
+        self.index = 0
+    
+    def showTM(self):
+        print(f"Q = {self.Q}")
+        print(f"E = {self.E}")
+        print(f"T = {self.T}")
+
+        print("TR = [")
+        for k in self.TR:
+            print(f"\t{k}: [")
+            for t in self.TR[k]:
+                print(f"\t\t{t},")
+            print("\t]")
+        print("]")
+
+        print(f"q0 = {self.q0}")
+        print(f"QACC = {self.QACC}")
+        print(f"QREJ = {self.QREJ}")
+        
+        
+def main(args):
+    assert os.path.exists(args), "Arquivo especificado nao existe."
+    m = TM(args)
+    m.showTM()
+    
 
 if __name__ == "__main__":
-    main()
+    assert len(sys.argv) != 1, "Arquivo de entrada nao especificado."
+    main(sys.argv[1])
